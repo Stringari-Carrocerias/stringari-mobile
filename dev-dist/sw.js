02c95d8b6e714f22da67bd3b259d2878810efc00
victor-de-sousa <victordesousaestudo@gmail.com>
2026-04-17 17:44:52 -0300

    feat: adiciona variáveis de ambiente e retorna imagem de modeloCarroceria

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.a5is06ekbes"
+    "revision": "0.u1ogpc66ft8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
