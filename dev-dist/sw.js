2f6d11a9ab6a0f8b594bda34df17f2d8df710624
victor-de-sousa <victordesousaestudo@gmail.com>
2026-04-22 10:39:56 -0300

    fix: erros de posicionamento do header e footer [ REFS: #12 ]

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-21a80088'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.g822canvrr8"
+    "revision": "0.vqtah816c4k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
