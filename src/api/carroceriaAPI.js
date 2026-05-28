95036ca7726574e57c6e150cf0a657da171538d2
victor-de-sousa <victordesousaestudo@gmail.com>
2026-05-28 16:49:27 -0300

    feat: adiciona filtragem por categoria para modelos de carroceria [ REFS: #16 ]

--- src/api/carroceriaAPI.js
+++ src/api/carroceriaAPI.js
@@ -1,20 +1,20 @@
 import apiClient from './config.js';
 
 const carroceriasApi = {
-  getAll() {
-    return apiClient.get('api/modelos%20carrocerias');
+  getAll(categoriaSelecionada) {
+    return apiClient.get(`api/modelosCarrocerias/?categoria=${categoriaSelecionada}`);
   },
 
   create(title) {
-    return apiClient.post('api/modelos%20carrocerias', { title });
+    return apiClient.post('api/modelosCarrocerias', { title });
   },
 
   update(id, data) {
-    return apiClient.patch(`api/modelos%20carrocerias/${id}`, data);
+    return apiClient.patch(`api/modelosCarrocerias/${id}`, data);
   },
 
   remove(id) {
-    return apiClient.delete(`api/modelos%20carrocerias/${id}`);
+    return apiClient.delete(`api/modelosCarrocerias/${id}`);
   },
 };
 
